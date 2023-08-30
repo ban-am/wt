9c3c46582c632e61c4887f37d42a76d63e6d9ef4
Matej Bandik <stbandk@gmail.com>
2023-08-30 21:38:56 +0200

    init pages

--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import '../app.css';
-</script>
-
-
-<div class="md:container md:mx-auto pt-4">
-	<slot />
-</div>
--- src/store.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-import type Resources from "@gitbeaker/core";
-
-
-export const clientStore = writable<Resources.Gitlab | undefined>(undefined);
